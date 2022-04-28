c2e95d8dacad025620cbc620023a48251c2ccff3
Simon <simon@ml-gis-service.com>
2022-04-28 15:03:15 +0300

    Load JSONL functions and tests

--- wsknn/utils/transform.py
+++ wsknn/utils/transform.py
@@ -1,3 +1,5 @@
+import gzip
+import json
 import pickle
 
 
@@ -16,3 +18,51 @@ def load_pickled(filename: str) -> dict:
     with open(filename, 'rb') as stored_data:
         pickled_object = pickle.load(stored_data)
     return pickled_object
+
+
+def load_jsonl(filename: str) -> dict:
+    """
+    Function loads data stored in JSON Lines.
+
+    Parameters
+    ----------
+    filename : str
+               Path to the file.
+
+    Returns
+    -------
+    : dict
+        Python dictionary with unique records.
+    """
+    datadict = {}
+    with open(filename, 'r') as fstream:
+        for fline in fstream:
+            pdict = json.loads(fline)
+            datadict.update(pdict)
+    return datadict
+
+
+def load_gzipped_jsonl(filename: str, encoding: str = 'UTF-8') -> dict:
+    """
+    Function loads data stored in gzipped JSON Lines.
+
+    Parameters
+    ----------
+    filename : str
+               Path to the file.
+
+    encoding : str, default = 'utf-8'
+
+    Returns
+    -------
+    : dict
+        Python dictionary with unique records.
+    """
+
+    datadict = {}
+
+    with gzip.open(filename, 'rt', encoding=encoding) as fstream:
+        for fline in fstream:
+            datadict.update(json.loads(fline))
+
+    return datadict
